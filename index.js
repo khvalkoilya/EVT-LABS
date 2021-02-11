28f193c12e3a50772bf6f1c090ac973088612a2d
Illia Khvalko <khvalko.ilya@gmail.com>
2021-02-12 00:56:13 +0300

    [1 laba] - create server with redirection on 403/404

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+const __dirname = path.resolve()
+const PORT = process.env.PORT || '8000';
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
+})
+
+app.get('/close', (req, res) => {
+  res.status(403);
+
+  res.sendFile(path.resolve(__dirname, 'static', 'errors', '403.html'));
+})
+
+app.get('/*', (req, res) => {
+  res.status(404);
+
+  res.sendFile(path.resolve(__dirname, 'static', 'errors', '404.html'));
+})
+
+app.listen(PORT, () => {
+  console.log('Server has been started at port: ', PORT);
+});
