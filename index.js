562ebe85e59fbc9ea156e1b6b6c0fe4b28ae77dc
Illia Khvalko <Illia_Khvalko@epam.com>
2021-04-10 14:06:44 +0300

    add regexp for email validation, save name+email in logs.txt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
 
@@ -26,6 +27,16 @@ app.get('/*', (req, res) => {
   res.status(404);
 
   res.sendFile(path.resolve(__dirname, 'static', 'errors', '404.html'));
+})
+
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.post('/accounts', (req, res) => {
+  const { name, email } = req.body;
+  fs.appendFileSync('logs.txt', `Account - email: ${email}, name: ${name};\n`)
+  res.redirect('/');
 })
 
 app.listen(PORT, () => {
